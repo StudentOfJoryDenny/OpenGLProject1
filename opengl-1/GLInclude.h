196e27ac7c27c9dfcc50e026ee043fa5f5797540
Dajun <Dajun@DESKTOP-7MOC686>
2021-10-08 22:27:21 -0700

    Add project files.

---
+++ opengl-1/GLInclude.h
@@ -0,0 +1,23 @@
+#ifndef _GL_INCLUDE_H_
+#define _GL_INCLUDE_H_
+
+// GL
+#define GL_GLEXT_PROTOTYPES
+#ifdef OSX
+#define GL_SILENCE_DEPRECATION
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+// GLM
+#include <glm/glm.hpp>
+// GLM Extensions. In a typical application you should not include all of them,
+// but rather just include what you need. GLM_ENABLE_EXPERIMENTAL allows for
+// many utilities that have been lightly tested, again this is not necessarily
+// recommended.
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/ext.hpp>
+
+#endif
+
